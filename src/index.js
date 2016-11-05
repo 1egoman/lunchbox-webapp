acfd4e9f03939ac2c7a37ef36e30666cd62c2a21
Ryan Gaus <rsg1egoman@gmail.com>
2016-11-05 10:52:41 -0400

    code: add redux to the app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, compose, applyMiddleware, combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+
 import App from './App';
-import './index.css';
+
+let store = createStore(combineReducers({
+  abc: () => 5,
+}), compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
