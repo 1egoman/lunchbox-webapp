143b22756d7755d1da81a5d7ebded67ce2327d2e
Ryan Gaus <rsg1egoman@gmail.com>
2016-11-12 12:19:49 -0500

    code: navbar styles and a new logo!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import {Link} from 'react-router';
+import classnames from 'classnames';
 
-export default function Navbar({children}) {
+export default function Navbar({router, children}) {
+  let path = router.location.pathname;
   return <div>
     <div className="app-navbar">
       <ul className="link-nav">
-        <li>Lunchbox</li>
-        <li><Link to="/items/">Links &amp; Items</Link></li>
-        <li><Link to="/calc">Calculated List</Link></li>
-        <li><Link to="/new">New...</Link></li>
+        {/* Nav branding stuff */}
+        <li className="nav-brand">
+          <img src="/images/logo.svg" role="presentation" />
+          <span>Lunchbox</span>
+        </li>
+        <li className={classnames({active: path.startsWith('/items')})}>
+          <Link to="/items/">Directory</Link>
+        </li>
+        <li className={classnames({active: path.startsWith('/grocery')})}>
+          <Link to="/grocery">Grocery List</Link>
+        </li>
+        <li className={classnames({active: path.startsWith('/pantry')})}>
+          <Link to="/pantry">Pantry</Link>
+        </li>
+
+        <li className="seperator">|</li>
+
+        <li className={classnames({active: path.startsWith('/calc')})}>
+          <Link to="/calc">Calculated List</Link>
+        </li>
+        <li className={classnames({active: path.startsWith('/new')})}>
+          <Link to="/new">New...</Link>
+        </li>
       </ul>
     </div>
     
