04a4bbeb4112caa4885b6f59e535cd878e751302
Ryan Gaus <rsg1egoman@gmail.com>
2016-11-11 20:42:51 -0500

    code: work on formatting calculated list page

--- src/components/CalculatedList.js
+++ src/components/CalculatedList.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+export function Price({price}) {
+  if (price.totalCost) {
+    return <span>
+      {price.totalCost} ({price.unitsRequired} at {price.pricePerUnit} each)
+    </span>;
+  } else {
+    return <span className="disabled">(no price)</span>;
+  }
+}
+
 export function CalculatedList({calculatedList}) {
   return <div className="app-calc-list">
     <h1>Calculated List</h1>
     <ul>
-      {calculatedList.map(i => <li key={i.item.name}>{i.item.name}</li>)}
+      {calculatedList.map(i => {
+        return <li key={i.item.name}>
+          <span className="name">{i.item.name}</span>
+          <span className="quantity">{i.item.quantity}</span>
+          <span className="price">
+            <Price price={i.price} />
+          </span>
+        </li>;
+      })}
     </ul>
   </div>;
 }
